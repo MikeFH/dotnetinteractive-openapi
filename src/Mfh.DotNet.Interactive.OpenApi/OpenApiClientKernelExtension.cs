892cc4f352264b97e11b8ca40fe21c3b9ca13267
MikeFH <MikeFH@users.noreply.github.com>
2021-06-19 17:28:09 +0200

    Ajoutez des fichiers projet.

---
+++ src/Mfh.DotNet.Interactive.OpenApi/OpenApiClientKernelExtension.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.CSharp;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
+
+namespace Mfh.DotNet.Interactive.OpenApi
+{
+    public class OpenApiClientKernelExtension : IKernelExtension
+    {
+        public OpenApiClientKernelExtension()
+        {
+
+        }
+
+        public Task OnLoadAsync(Kernel kernel)
+        {
+            Command command = new Command("#!openapi-client", "Generate an api client based on its OpenAPI schema");
+            command.AddArgument(new Argument("schema"));
+
+            command.Handler = CommandHandler.Create<string, KernelInvocationContext>(async (schema, invocationContext) => await GenerateClient(schema, invocationContext));
+
+            kernel.AddDirective(command);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task GenerateClient(string schema, KernelInvocationContext invocationContext)
+        {
+            CSharpKernel csharpKernel = null;
+
+            invocationContext.HandlingKernel.VisitSubkernelsAndSelf(k =>
+            {
+                if (k is CSharpKernel csk)
+                {
+                    csharpKernel = csk;
+                }
+            });
+
+            if (csharpKernel is null)
+            {
+                return;
+            }
+
+            string clientClassName = "OpenApiClient";
+
+            DisplayedValue dv = invocationContext.Display((object)i("Downloading schema..."), "text/html");
+
+            OpenApiDocument document = await OpenApiDocument.FromUrlAsync(schema);
+
+            dv.Update((object)i("Building client..."));
+
+            CSharpClientGeneratorSettings settings = new CSharpClientGeneratorSettings()
+            {
+                ClassName = clientClassName,
+                ExposeJsonSerializerSettings = true,
+                CSharpGeneratorSettings =
+                {
+                    Namespace = "DummyNamespace",
+                    GenerateDataAnnotations = false
+                }
+            };
+
+            CSharpClientGenerator generator = new CSharpClientGenerator(document, settings);
+            string clientCode = generator.GenerateFile();
+
+            //Remove #pragma diirectives useless in this context and the namespace to make the code usable in a script context
+            clientCode = Regex.Replace(clientCode, "^(#pragma|//).*$", "", RegexOptions.Multiline);
+            clientCode = clientCode
+                .Replace("namespace DummyNamespace", "")
+                .Trim()
+                .Trim(new[] { '{', '}' });
+
+            //Extend the client functionnality
+            clientCode += Environment.NewLine;
+            clientCode += $@"public partial class {clientClassName} {{
+                private static readonly System.Net.Http.HttpClient DefaultHttpClient = new System.Net.Http.HttpClient();
+
+                public {clientClassName}() 
+                    :this(DefaultHttpClient) {{ }}
+
+                partial void ProcessResponse(System.Net.Http.HttpClient client, System.Net.Http.HttpResponseMessage response) {{
+                    display(response);
+                }}
+            }}";
+
+            await csharpKernel.SubmitCodeAsync(@"#r ""System.ComponentModel.DataAnnotations""");
+            await csharpKernel.SubmitCodeAsync(clientCode);
+
+            dv.Update((object)
+                details(
+                    summary(
+                        "Client generated for " + schema
+                    ),
+                    p(
+                        "You can intialize the client as shown below",
+                        br(),
+                        code($"var client = new {clientClassName}()"),
+                        br(),
+                        "or using an already existing ",
+                        code("HttpClient"),
+                        br(),
+                        code(
+                            "var httpClient = new HttpClient();",
+                            br(),
+                            $"var client = new {clientClassName}(httpClient);"
+                        )
+                    )
+                )
+            );
+        }
+    }
+}

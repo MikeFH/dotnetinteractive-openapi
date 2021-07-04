685089b2e89124d0be68b9b5ca3c65a54008b363
MikeFH <MikeFH@users.noreply.github.com>
2021-07-04 15:57:18 +0200

    Create test project

---
+++ src/MfhSoft.DotNet.Interactive.OpenApi.Tests/OpenApiClientKernelExtensionTest.cs
@@ -0,0 +1,211 @@
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.CSharp;
+using FluentAssertions;
+using Xunit;
+using Microsoft.DotNet.Interactive.Events;
+using System.Threading.Tasks;
+
+namespace MfhSoft.DotNet.Interactive.OpenApi.Tests
+{
+    public class OpenApiClientKernelExtensionTest
+    {
+        private const string RemoteYamlSchemaUrl = @"https://petstore.swagger.io/v2/swagger.yaml";
+        private const string RemoteJsonSchemaUrl = @"https://petstore.swagger.io/v2/swagger.json";
+
+        private const string LocalYamlSchemaPath = @".\schemas\petstore.yaml";
+        private const string LocalJsonSchemaPath = @".\schemas\petstore.json";
+
+        private async Task<CSharpKernel> CreateKernel()
+        {
+            CSharpKernel kernel = new CSharpKernel().UseNugetDirective();
+            //using the local Newtonsoft.Json because package restoration doesn't seem to work in this context
+            //https://github.com/dotnet/interactive/issues/1055
+            await kernel.SubmitCodeAsync(@"#r ""Newtonsoft.Json.dll""");
+
+            OpenApiClientKernelExtension extension = new OpenApiClientKernelExtension();
+            await extension.OnLoadAsync(kernel);
+
+            return kernel;
+        }
+
+        [Fact]
+        public async Task CanLoadLocalYamlSchema()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalYamlSchemaPath}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+        }
+
+        [Fact]
+        public async Task CanLoadLocalJsonSchema()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalJsonSchemaPath}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+        }
+
+        [Fact]
+        public async Task CanLoadRemoteYamlSchema()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{RemoteYamlSchemaUrl}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+        }
+
+        [Fact]
+        public async Task CanLoadRemoteJsonSchema()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{RemoteJsonSchemaUrl}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+        }
+
+        [Fact]
+        public async Task GeneratesAClassWithTwoConstructors()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalJsonSchemaPath}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+
+            KernelCommandResult result2 = await kernel.SubmitCodeAsync("new OpenApiClient()");
+
+            result2.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+
+            KernelCommandResult result3 = await kernel.SubmitCodeAsync("new OpenApiClient(new System.Net.Http.HttpClient())");
+
+            result3.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+        }
+
+        [Fact]
+        public async Task CanCustomizeClassName()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalJsonSchemaPath}"" -c ""CustomName""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+
+            KernelCommandResult result2 = await kernel.SubmitCodeAsync("new CustomName()");
+
+            result2.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+        }
+
+        [Fact]
+        public async Task HasMethodNameTypePath()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalJsonSchemaPath}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+
+            KernelCommandResult result2 = await kernel.SubmitCodeAsync(@"typeof(OpenApiClient).GetMethods().Any(mi => mi.Name == ""PetGetAsync"")");
+
+            result2.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors()
+                .And
+                .ContainSingle<ReturnValueProduced>()
+                .Which
+                .Value
+                .As<bool>()
+                .Should()
+                .Be(true);
+        }
+
+        [Fact]
+        public async Task HasMethodNameTypeOperationId()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalJsonSchemaPath}"" --method-name-type operationid");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+
+            KernelCommandResult result2 = await kernel.SubmitCodeAsync(@"typeof(OpenApiClient).GetMethods().Any(mi => mi.Name == ""GetPetByIdAsync"")");
+
+            result2.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors()
+                .And
+                .ContainSingle<ReturnValueProduced>()
+                .Which
+                .Value
+                .As<bool>()
+                .Should()
+                .Be(true);
+        }
+
+        [Fact]
+        public async Task ModelHasToJsonMethod()
+        {
+            CSharpKernel kernel = await CreateKernel();
+
+            KernelCommandResult result = await kernel.SubmitCodeAsync($@"#!openapi-client ""{LocalJsonSchemaPath}""");
+
+            result.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors();
+
+            KernelCommandResult result2 = await kernel.SubmitCodeAsync(@"typeof(Pet).GetMethods().Any(mi => mi.Name == ""ToJson"")");
+
+            result2.KernelEvents
+                .ToSubscribedList()
+                .Should()
+                .NotContainErrors()
+                .And
+                .ContainSingle<ReturnValueProduced>()
+                .Which
+                .Value
+                .As<bool>()
+                .Should()
+                .Be(true);
+        }
+    }
+}
---
+++ src/MfhSoft.DotNet.Interactive.OpenApi.Tests/TestUtility.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using FluentAssertions.Collections;
+using FluentAssertions.Primitives;
+using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Server;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace MfhSoft.DotNet.Interactive.OpenApi.Tests
+{
+    public static class AssertionExtensions
+    {
+        public static AndConstraint<GenericCollectionAssertions<KernelEvent>> NotContainErrors(
+            this GenericCollectionAssertions<KernelEvent> should) =>
+            should
+                .NotContain(e => e is ErrorProduced)
+                .And
+                .NotContain(e => e is CommandFailed);
+
+        public static AndWhichConstraint<ObjectAssertions, T> ContainSingle<T>(
+            this GenericCollectionAssertions<KernelEvent> should,
+            Func<T, bool> where = null)
+            where T : KernelEvent
+        {
+            T subject;
+
+            if (where is null)
+            {
+                should.ContainSingle(e => e is T);
+
+                subject = should.Subject
+                                .OfType<T>()
+                                .Single();
+            }
+            else
+            {
+                should.ContainSingle(e => e is T && where((T)e));
+
+                subject = should.Subject
+                                .OfType<T>()
+                                .Where(where)
+                                .Single();
+            }
+
+            return new AndWhichConstraint<ObjectAssertions, T>(subject.Should(), subject);
+        }        
+    }
+
+    public static class ObservableExtensions
+    {
+        public static SubscribedList<T> ToSubscribedList<T>(this IObservable<T> source)
+        {
+            return new SubscribedList<T>(source);
+        }
+    }
+
+    public class SubscribedList<T> : IReadOnlyList<T>, IDisposable
+    {
+        private ImmutableArray<T> _list = ImmutableArray<T>.Empty;
+        private readonly IDisposable _subscription;
+
+        public SubscribedList(IObservable<T> source)
+        {
+            _subscription = source.Subscribe(x => { _list = _list.Add(x); });
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_list).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public int Count => _list.Length;
+
+        public T this[int index] => _list[index];
+
+        public void Dispose() => _subscription.Dispose();
+    }
+}
